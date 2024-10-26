91482a0aa773893788366ce698000606bb0c34a4
yllee9127 <yllee9127@gmail.com>
2024-10-26 21:45:57 +0800

    Modified app.py

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, Yoon Long!</p>"
 
+@app.route("/happy")
+def happy_world():
+    return "<p>Happy !</p>"
+
+@app.route("/sad")
+def sad_world():
+    return "<p>Sad !</p>"
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080)
